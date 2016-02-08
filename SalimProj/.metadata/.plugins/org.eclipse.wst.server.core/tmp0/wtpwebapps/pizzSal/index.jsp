391b6fba0612b30425d59bd0ecb7994582f12230
SenhajiMahmoud <sema_ma@hotmail.fr>
2016-02-08 14:25:10 +0100

    azr

--- SalimProj/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/pizzSal/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2><a href="nouveauProduit.html">Ajouter</a></h2>
-</body>
-</html>
--- SalimProj/pizzSal/src/main/java/test/hibernate/test/TestMain.java
+++ SalimProj/pizzSal/src/main/java/test/hibernate/test/TestMain.java
@@ -8,8 +8,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		PersonneManager pm = new PersonneManager();
 		pm.ajout("Senhaji", "Mahmoud", "0666065573", "senhaji.mahmoud.mi@gmail.com");
-//		pm.ajout("Mimid", "Mahmoud", "0666065573", "senhaji.mahmoud.mi@gmail.com");
-//		pm.ajout("sss", "Mahmoud", "0666065573", "sema_ma@hotmail.fr");
+		pm.ajout("Mimid", "Mahmoud", "0666065573", "senhaji.mahmoud.mi@gmail.com");
+		pm.ajout("sss", "Mahmoud", "0666065573", "sema_ma@hotmail.fr");
 		HibernateUtil.sessionFactory.close();
 	}
 
